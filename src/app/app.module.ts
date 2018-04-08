5416c713592fc777d57cf3925a357916b1adf97f
Sasha Masliuk <777mmm@ukr.net>
2018-04-08 20:22:14 +0300

    [bm] add Auth component review: myself

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FormsModule,
+    // HttpModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bm-auth',
+    templateUrl: './auth.component.html'
+})
+
+export class AuthComponent {
+
+}
